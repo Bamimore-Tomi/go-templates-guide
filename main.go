23d88c7b6c3a01d8033caad2714c8825be50d370
unknown <tomibami2020@gmail.com>
2021-10-03 22:23:08 +0100

    Execute the variable into the template

--- main.go
+++ main.go
@@ -6,28 +6,26 @@ import (
 	"text/template"
 )
 
-// Parse all the files in a certain directory
-func main() {
-	// This function takes a pattern. It can be a folder
-	temp, err := template.ParseGlob("templates/*")
+// Declare type pointer to a template
+var temp *template.Template
 
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// Simply calling execute parses the first file in the directory
-	err = temp.Execute(os.Stdout, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// Or you can execute a particular template in the directory
-	err = temp.ExecuteTemplate(os.Stdout, "template-03.txt", nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// Calling a template not in the directory will produce an error
-	err = temp.ExecuteTemplate(os.Stdout, "template-04.txt", nil)
+// Using the init function to make sure the template is only parsed once in the program
+func init() {
+	// template.Must takes the reponse of template.ParseFiles and does error checking
+	temp = template.Must(template.ParseFiles("template-04.txt"))
+}
+
+func main() {
+	// Execute myName into the template and  print to Stdout
+	myName := "Oluwatomisin"
+	err := temp.Execute(os.Stdout, myName)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 }
+
+// Hello Oluwatomisin
+
+// You are doing great, keep learning.
+// Do not stop Oluwatomisin
