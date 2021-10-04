5ecb980ab7d395caa82a592f02c598804e87bd52
unknown <tomibami2020@gmail.com>
2021-10-04 12:29:20 +0100

    Varibles in templates

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 	"text/template"
 )
 
+type PersonTrait struct {
+	Name  string
+	Trait string
+}
+
 // Declare type pointer to a template
 var temp *template.Template
 
 // Using the init function to make sure the template is only parsed once in the program
 func init() {
 	// template.Must takes the reponse of template.ParseFiles and does error checking
-	temp = template.Must(template.ParseFiles("template-04.txt"))
+	temp = template.Must(template.ParseFiles("template-05.txt"))
 }
 
 func main() {
-	// Execute myName into the template and  print to Stdout
-	myName := "Oluwatomisin"
-	err := temp.Execute(os.Stdout, myName)
+	// Execute person into the template and  print to Stdout
+	person := PersonTrait{Name: "Oluwatomisin", Trait: "a great writer"}
+	err := temp.Execute(os.Stdout, person)
 	if err != nil {
 		log.Fatalln(err)
 	}
